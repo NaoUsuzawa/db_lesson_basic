e0632659067151c819035d91af8d698b8e74e850
NaoUsuzawa <cookieandnao@gmail.com>
2026-02-06 15:36:25 +0900

    initial commit

---
+++ db_lesson.sql
@@ -0,0 +1,9 @@
+INSERT INTO people (name, email, age, gender)
+    -> VALUES
+    -> ('田中ゆうこ', 'tanala@gizumo.jp', 25, 2),
+    -> ('福田だいすけ', 'fukuda@gizumo.jp', 42, 1),
+    -> ('豊田はなこ' , 'toyoshima@gizumo.jp', 34, 2),
+    -> ('早坂てつお', 'hayasaka@gizumo.co.jp', 61, 1),
+    -> ('不思議沢みちこ', NULL, NULL, NULL);
+
+    
